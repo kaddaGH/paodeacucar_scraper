61af604c58434e9ccbd50902d05e449e8aa4dc8a
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-18 20:07:35 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -17,7 +17,7 @@ search_terms.each do |search_term|
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://paodeacucar.resultspage.com/search?p=Q&ts=json-full&lot=json&w=#{CGI.escape(search_term)}&cnt=12&ref=www.paodeacucar.com&ua=Mozilla%2F5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_10_0)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F72.0.3626.109%20Safari%2F537.36&ep.selected_store=501",
+      url: "https://paodeacucar.resultspage.com/search?p=Q&ts=json-full&lot=json&w=#{CGI.escape(search_term)}&cnt=12&srt=0&ref=www.paodeacucar.com&ua=Mozilla%2F5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_10_0)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F72.0.3626.109%20Safari%2F537.36&ep.selected_store=501",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
