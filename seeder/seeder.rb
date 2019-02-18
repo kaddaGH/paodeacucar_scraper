d40d12fa5a9af38a639270dab9b957dc4ef39c11
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-18 13:18:08 +0100

    Update parser.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -12,8 +12,8 @@ pages << {
 
 }
 search_terms = ['Red Bull', 'RedBull', 'Energético', 'Energéticos']
-search_terms.each do |search_term|
-  break
+search_terms.take(1).each do |search_term|
+
   pages << {
       page_type: 'products_search',
       method: 'GET',
